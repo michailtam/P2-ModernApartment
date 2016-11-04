c0f8851715b53b267b7be82b78b4097af9d54a05
Michael <mike.gr002@gmail.com>
2016-11-04 02:03:28 +0200

    Spinning Globe is working !!!

--- Assets/VRND-Course2-Project/Scripts/TriggerAnimation.cs
+++ Assets/VRND-Course2-Project/Scripts/TriggerAnimation.cs
@@ -2,32 +2,38 @@
 using System.Collections;
 
 public class TriggerAnimation : MonoBehaviour {
-    public string AnimationName;
+	public string firstTrigger;
+	public string secondTrigger;
     public Animator stateMachine;
 	public GvrViewer vrViewer;
 
     private bool created = false;
 
     void Awake() {
-        /*if (GvrViewer.Instance == null) {
-            GvrViewer.Create();
-            created = true;
-			Debug.Log ("VR Camera created: " + vrViewer); 
-        }*/
+		// IMPORTANT: Changed cause the existing vr camera was overwriten
+		if (vrViewer.GetInstanceID() == null) {
+			GvrViewer.Create();
+            created = true; 
+		} 
     }
 
     void Start() {
         if (created) {
-            foreach (Camera c in GvrViewer.Instance.GetComponentsInChildren<Camera>()) {
+			foreach (Camera c in GvrViewer.Instance.GetComponentsInChildren<Camera>()) {
                 c.enabled = false; // to use the Gvr SDK without adding cameras we have to disable them
             }
         }
     }
 
     void Update() {
-        GvrViewer.Instance.UpdateState(); //need to update the data here otherwise we dont get mouse clicks; this is because we are automatically creating the GVRSDK (seems like a bug)
-        if (GvrViewer.Instance.Triggered)
-            stateMachine.SetTrigger(AnimationName);
-    }
+		GvrViewer.Instance.UpdateState(); //need to update the data here otherwise we dont get mouse clicks; this is because we are automatically creating the GVRSDK (seems like a bug)
+		if (GvrViewer.Instance.Triggered) {
 
+			if (stateMachine.GetCurrentAnimatorStateInfo (0).IsName ("GlobeIdle")) {
+				stateMachine.SetTrigger (firstTrigger);
+			} else {
+				stateMachine.SetTrigger (secondTrigger);
+			}
+		}
+    }
 }
